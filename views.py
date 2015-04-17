c60188a2c0e408fbb3ad5bb83a4961a41e19856e
Gao Yang <kingjiyang@gmail.com>
2015-04-18 11:37:21 +1200

    fixed click event bug.

--- views.py
+++ views.py
@@ -48,7 +48,7 @@ def wechat():
 
     if ret['type'] == 'event' and 'event' in _wechat._hooks_mapping:
         func = _wechat._hooks_mapping['event'] 
-    if ret['content'].startswith('trademe#') and 'trademe' in _wechat.hooks_mapping:
+    elif ret['content'].startswith('trademe#') and 'trademe' in _wechat.hooks_mapping:
         func = _wechat._hooks_mapping['trademe']
     else:
         if '*' in _wechat._hooks_mapping:
