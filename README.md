c0a9ca10e5887e6b87341663925dad92ffd259bc
Gao Yang <kingjiyang@gmail.com>
2015-04-23 20:31:27 +1200

    initial commit.

---
+++ README.md
@@ -0,0 +1,12 @@
+## WeChat message gateway
+
+== Perpose ==
+  1. Simplifying response from WeChat API
+  2. Spliting backend process to module function
+  3. Tidying code up.
+
+== Framework and backend ==
+
+* Twisted
+* Celery
+* Redies
--- candy/upload.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-curl -F media=@medias/IMG_9393.jpg "http://file.api.weixin.qq.com/cgi-bin/media/upload?access_token=5r_2Pjpqjk-P1gtZX46Mpp3yw44IjidCsEcMW2LUNtePa3y-xfhD0N_NFo5SEgMEn--P2GbY45Cdj41YEQ7suRH8_hZTFZOxaodA-eTsCVQ&type=image"
